8cc47af86c0607d25aba66c1656d43844e2c0028
JuanJesuses <juanj.rodriguez.sanchez@gmail.com>
2023-01-16 13:44:40 +0100

    Continúo hasta los primeros ejercicios del proyecto del juego. Pág. 238. Ejercicios pendientes de terminar.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Clase general para gestionar los activos y el comportamiento del juego."""
@@ -15,19 +16,28 @@ class AlienInvasion:
             (self.settings.screen_witdh, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Inicia el bucle inicial para el juego."""
         while True:
             # Vigila los eventos de ratón y teclado.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # Redibuja la pantalla a cada paso por el bucle.
-            self.screen.fill(self.settings.bg_color)
+    def _check_events(self):
+        """Responde a pulsaciones de teclas y eventos de ratón."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            # Hace visible la pantalla dibujada más recientemente.
-            pygame.display.flip()
+    def _update_screen(self):
+        """Actualiza las imágenes en la pantalla y cambia a la pantalla nueva."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Hace visible la pantalla dibujada más recientemente.
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Crea una instancia del juego y lo ejecuta.
